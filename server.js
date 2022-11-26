17db156eccc00cc8838e37c24d1cc5a30dee0dbb
Stanley Mair <stanleygmair@gmail.com>
2022-11-26 13:55:11 +1300

    Added 2 get routes for quotes

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const { quotes } = require('./data');
-const { getRandomElement } = require('./utils');
+const { quotes } = require("./data");
+const { getRandomElement } = require("./utils");
 
 const PORT = process.env.PORT || 4001;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
+app.get("/api/quotes", (req, res) => {
+	res.send(quotes);
+});
+
+app.get("/api/quotes/random", (req, res) => {
+	res.send(getRandomElement(quotes));
+});
+
+app.listen(PORT, () => {
+	console.log(`Listening on localhost:${PORT}`);
+});
